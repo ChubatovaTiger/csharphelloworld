0e2809aadbcbb667a55ce2b6d283d5c4e2810b24
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-02 19:44:54 +0200

    Update Program.cs

--- csharphelloworld/Program.cs
+++ csharphelloworld/Program.cs
@@ -6,7 +6,7 @@ namespace csharphelloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello...World!");
+            Console.WriteLine("Hello…World!");
         }
     }
 }
